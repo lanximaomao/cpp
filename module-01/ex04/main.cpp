20d31a4447e59997caa1c2b8270eadba2f5d7560
Lin <lsun@student.hive.fi>
2023-08-01 20:04:34 +0300

    wip

--- module-01/ex04/main.cpp
+++ module-01/ex04/main.cpp
@@ -1,59 +1,28 @@
-#include "Replace.hpp"
+#include <iostream>
+#include <fstream>
 
-//#include "Replace.hpp"
-// memory leak?
-static int if_match(std::string buf, std::string target)
+std::string		replace(std::string line, std::string old_str, std::string new_str)
 {
-	for (size_t i = 0; i < buf.length(); i++)
+	int pos = 0;
+
+	//std::cout << line << std::endl;
+	//std::cout << old_str << std::endl;
+	//std::cout << new_str << std::endl;
+	while (line.find(old_str, pos) != std::string::npos)
 	{
-		//std::cout << "buf=" << buf << "; len=" << buf.length() << std::endl;
-		for (size_t j = 0; j < target.length() - 1; j++)
-		{
-			//std::cout << i << " && " << j << std::endl;
-			if (buf[i+j] != target[j])
-				break;
-			if (j == target.length() - 2)
-			{
-				//std::cout << i << " && " << j << std::endl;
-				return (i);
-			}
-		}
+		line.erase(pos, old_str.length());
+		std::cout << line << std::endl;
+		line.insert(pos, new_str);
+		std::cout << line << std::endl;
+		pos += new_str.length();
+		std::cout << "pos=" << pos << std::endl;
 	}
-	return (-1);
-}
-
-// back gives the last character in a string
-// what if the replaced string is a null?
-void		Replace::callReplace(std::ifstream *sourceFile, std::ofstream *destFile)
-{
-	std::string buf;
-	char lastChar = this->old_str.back();
-
-	while (std::getline(*sourceFile, buf, lastChar))
-	{
-		//std::cout << "buf=" << buf << std::endl;
-		if (buf.length() + 1 >= this->old_str.length())
-		{
-			int i = if_match(buf, this->old_str);
-			if (i != -1)
-			{
-				std::string before;
-
-				//std::cout << "match! buf = " << buf << std::endl;
-				if (i > 0)
-					before = buf.substr(0, i);
-				else
-					before = new_str;
-				*destFile << before.append(new_str);
-			}
-		}
-		else
-			*destFile << buf << lastChar;
-	}
+	return(line);
 }
 
 // handel unexpected inputs and errors
-// std::ios::out | std::ios::trunc means if the file should be open for output and if the file exisit, it should be overwritten.
+// std::ios::out | std::ios::trunc means if the file should be open for output and if the file exisit
+// it should be overwritten.
 int main(int argc, char** argv)
 {
 	if (argc != 4)
@@ -61,41 +30,38 @@ int main(int argc, char** argv)
 		std::cout << "wrong number of arguments." << std::endl;
 		return (1);
 	}
-	//parsing
-	Replace sed;
+	std::string line;
+	std::string filename = std::string(argv[1]);
+	std::string old_str = std::string(argv[2]);
+	std::string new_str = std::string (argv[3]);
+	std::ifstream sourceFile(filename);
 
-	sed.filename = std::string(argv[1]);
-	sed.old_str = std::string(argv[2]);
-	sed.new_str = std::string(argv[3]);
-
-	//check if filename is valid
-
-	std::ifstream sourceFile(sed.filename);
-	if (sourceFile.is_open())
+	if (sourceFile)
 	{
-		// create a new file for output
-		//what if this file exi
-		std::ofstream destFile(sed.filename.append(".replace"), std::ios::out | std::ios::trunc);
-		if (destFile.is_open())
+		std::ofstream destFile(filename.append(".replace"));
+		if (destFile)
 		{
-			// copy file
-			//destFile << sourceFile.rdbuf();// copy the content
-			sed.callReplace(&sourceFile, &destFile);
+			while (getline(sourceFile,line))
+			{
+
+				line.append("\n");
+				line = replace(line, old_str, new_str);
+				destFile << line;
+			}
 		}
 		else
 		{
 			std::cout << "Failed to create the file." << std::endl;
-			sourceFile.close(); // close sourceFile
-			std::exit(1); // error code?
+			sourceFile.close();
+			std::exit(1);
 		}
-		//close two files
 		sourceFile.close();
 		destFile.close();
 	}
-	else // fail to open
+	else
 	{
-		std::cout << "Failed to open the file." << std::endl;
-		std::exit(1);
+		std::cout << "Fail to open source file.\n";
+		return (1);
 	}
 	return (0);
 }
