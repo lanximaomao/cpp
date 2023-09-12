dbd56ec6844c0179609d14ccc78dd8a453aee6e8
lanximaomao <linlin.sun@outlook.com>
2023-09-12 17:36:04 +0300

    wip: module8/ex02

--- module-08/ex02/MutantStack.hpp
+++ module-08/ex02/MutantStack.hpp
@@ -3,34 +3,29 @@
 
 #include <iostream>
 #include <stack>
+#include <deque>
 
 template <typename T>
-class MutantStack: public std::stack
+class MutantStack : public std::stack<T, std::deque<T> >
 {
-	private:
-		T*		_ptr; // raw pointer
 	public:
 		MutantStack();
-		// copy constructor needed or not?
+		MutantStack(const MutantStack& other);
+		MutantStack& operator=(const MutantStack& other);
 		~MutantStack();
-		class iterator
-		{
-			T* operator=(T* other); //
-			int operator++(T* other);
-			int operator--(T* other);
-			void operator++(T* other);
-			void operator--(T* other);
-			T* operator+(T* other);
-			T* operator-(T* other);
-			bool operator!=(T* other);
-			bool operatpr==(T* other);
-		};
 
-		class const_iterator
-		{
-			T& const_iterator();
-		}
-}
+		using iterator = typename std::deque<T>::iterator;
+		using const_iterator = typename std::deque<T>::const_iterator;
+
+		iterator begin() { return std::deque<T>::begin(); }
+		iterator end() { return std::deque<T>::end(); }
+		iterator rbegin() { return std::deque<T>::rbegin(); }
+		iterator rend() { return std::deque<T>::rend(); }
+		const_iterator cbegin() const { return std::deque<T>::cbegin(); }
+		const_iterator cend() const { return std::deque<T>::cend(); }
+		const_iterator crbegin() const { return std::deque<T>::crbegin(); }
+		const_iterator crend() const { return std::deque<T>::crend(); }
+};
+
 
 # endif
-
